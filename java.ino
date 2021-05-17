292f67e6d1bd986f7e347f70dc521dfbc8f622a2
Bram <74303914+BramGithub@users.noreply.github.com>
2021-05-17 19:01:27 +0200

    Add files via upload

--- java.ino
+++ java.ino
@@ -1,35 +1,30 @@
 char incomingByte; // for incoming serial data
-String toon = "";
-
+String musictone = "";
 void setup() {
   Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
-  pinMode(13, INPUT); // left button
-  pinMode(12, INPUT); // right button
-  pinMode(A5, INPUT); // lightsensor
-  pinMode(3, OUTPUT);
-  pinMode(11, OUTPUT); // light
-  pinMode(10,OUTPUT); // speaker
+  pinMode(13, INPUT);//leftbutton
+  pinMode(12, INPUT);//rightbutton
+  pinMode(A5, INPUT);//lightsensor
+  pinMode(11, OUTPUT);//light
+  pinMode(10, OUTPUT); //speaker
 }
-
 long previousmillis = 0;
 
 void loop() {
   incomingByte = Serial.read();
 
+  //Plays music
   if (incomingByte != 'X' && incomingByte >= 0) {
-    toon = toon + incomingByte;
+    musictone = musictone + incomingByte;
   }
-  
   if (incomingByte == 'X') {
-    if(toon.toInt()!=0){
-    tone(10, toon.toInt());}
-    else{noTone(10);}
-    toon = "";
-  }
-
-  if(incomingByte=='W'){
-    Serial.print(analogRead(A5));
-    digitalWrite(11,HIGH);
+    if (musictone.toInt() != 0) {
+      tone(10, musictone.toInt());
+    }
+    else {
+      noTone(10);
+    }
+    musictone = "";
   }
 
   //controller
@@ -43,11 +38,13 @@ void loop() {
     Serial.print("D");
   }
 
+  
+  //Refreshes the newest lightsensor data every second
   int lichtsterkte = analogRead(A5);
-  //verstuurlichtsterkte
   unsigned long currentmillis = millis();
-  if (currentmillis - previousmillis > 6000) {
+  if (currentmillis - previousmillis > 1000) {
     previousmillis = millis();
+    Serial.flush();
     Serial.print(lichtsterkte);
   }
 }
