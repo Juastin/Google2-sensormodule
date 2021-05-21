59fd6a079b2f0b89f38d69f7ec0404a8e272874f
Bram <74303914+BramGithub@users.noreply.github.com>
2021-05-21 17:26:42 +0200

    Add files via upload

--- java.ino
+++ java.ino
@@ -4,19 +4,19 @@ bool lightOn;
 
 
 void setup() {
-  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+  Serial.begin(9600);// opens serial port, sets data rate to 9600 bps
   pinMode(13, INPUT);//leftbutton
   pinMode(12, INPUT);//rightbutton
   pinMode(A5, INPUT);//lightsensor
   pinMode(11, OUTPUT);//light
-  pinMode(10, OUTPUT); //speaker
+  pinMode(10, OUTPUT);//speaker
 }
 long previousmillis = 0;
 
 void loop() {
   incomingByte = Serial.read();
-
-
+  
+//Turns light on if lightvalue is too low
   if(incomingByte == -20){
       digitalWrite(11, HIGH);
     } else if(incomingByte == -10){
@@ -41,10 +41,6 @@ void loop() {
   int lichtsterkte = analogRead(A5);
   if(incomingByte == 'W'){
     Serial.print(lichtsterkte);}
-
-    if(incomingByte == -30){
-    Serial.print(lichtsterkte);
-    digitalWrite(11,HIGH);}
 
   //controller
   if (digitalRead(13) == HIGH && digitalRead(12) == HIGH) {
